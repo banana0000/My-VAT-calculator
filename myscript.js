26c2e7396cb270ce2e38dc12776007bb774effe1
Eszter Kovacs <koveszter7@gmail.com>
2022-04-22 14:15:38 +0200

    add website

---
+++ myscript.js
@@ -0,0 +1,20 @@
+var netInput = document.getElementById('net'),
+    NET = document.getElementById('vat'),
+    Gross = document.getElementById('res'),
+    btn = document.getElementById('gomb'),
+    netFunction;
+
+
+sumFunction = function() {
+  res.value = (net.value * (vat.value / 100 + 1));
+} 
+
+btn.addEventListener('click', sumFunction);
+
+
+(function() {
+  net.Function.focus();
+  btn.click();
+});
+
+
